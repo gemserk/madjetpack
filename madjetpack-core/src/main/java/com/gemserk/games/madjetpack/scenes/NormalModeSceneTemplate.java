faaeb4cee9cc409c8de90c5f2aeb981cd22c750e
Ariel Coppes <ariel.coppes@gemserk.com>
2011-09-17 12:42:44 -0300

    added a character with a jetpack (use your imagination) moved with wasd

--- madjetpack-core/src/main/java/com/gemserk/games/madjetpack/scenes/NormalModeSceneTemplate.java
+++ madjetpack-core/src/main/java/com/gemserk/games/madjetpack/scenes/NormalModeSceneTemplate.java
@@ -3,10 +3,14 @@ package com.gemserk.games.madjetpack.scenes;
 import com.artemis.Entity;
 import com.artemis.World;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.Body;
 import com.badlogic.gdx.physics.box2d.BodyDef.BodyType;
 import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
 import com.gemserk.commons.artemis.WorldWrapper;
+import com.gemserk.commons.artemis.components.PhysicsComponent;
+import com.gemserk.commons.artemis.components.ScriptComponent;
 import com.gemserk.commons.artemis.events.EventManager;
 import com.gemserk.commons.artemis.events.EventManagerImpl;
 import com.gemserk.commons.artemis.render.RenderLayers;
@@ -18,10 +22,12 @@ import com.gemserk.commons.artemis.systems.ScriptSystem;
 import com.gemserk.commons.artemis.systems.TagSystem;
 import com.gemserk.commons.artemis.templates.EntityFactory;
 import com.gemserk.commons.artemis.templates.EntityFactoryImpl;
+import com.gemserk.commons.artemis.templates.EntityTemplate;
 import com.gemserk.commons.artemis.templates.EntityTemplateImpl;
 import com.gemserk.commons.gdx.box2d.BodyBuilder;
 import com.gemserk.commons.gdx.camera.Libgdx2dCamera;
 import com.gemserk.commons.gdx.camera.Libgdx2dCameraTransformImpl;
+import com.gemserk.commons.gdx.games.PhysicsImpl;
 import com.gemserk.componentsengine.utils.ParametersWrapper;
 import com.gemserk.games.madjetpack.GameInformation;
 import com.gemserk.games.madjetpack.components.RenderScriptComponent;
@@ -29,8 +35,8 @@ import com.gemserk.games.madjetpack.systems.RenderScriptSystem;
 import com.gemserk.resources.ResourceManager;
 
 public class NormalModeSceneTemplate {
-	
-	class Box2dRenderDebugScript extends ScriptJavaImpl {
+
+	static class Box2dRenderDebugScript extends ScriptJavaImpl {
 
 		private final Box2DDebugRenderer box2dDebugRenderer = new Box2DDebugRenderer();
 		private final Libgdx2dCamera camera;
@@ -48,6 +54,39 @@ public class NormalModeSceneTemplate {
 
 	}
 
+	static class CharacterControllerScript extends ScriptJavaImpl {
+
+		public CharacterControllerScript() {
+
+		}
+
+		@Override
+		public void update(World world, Entity e) {
+
+			Vector2 movementDirection = new Vector2(0f, 0f);
+
+			if (Gdx.input.isKeyPressed(Keys.D)) {
+				movementDirection.x += 1f;
+			} else if (Gdx.input.isKeyPressed(Keys.A)) {
+				movementDirection.x -= 1f;
+			}
+
+			if (Gdx.input.isKeyPressed(Keys.W)) {
+				movementDirection.y += 1f;
+			}
+
+			PhysicsComponent physicsComponent = e.getComponent(PhysicsComponent.class);
+
+			Body body = physicsComponent.getBody();
+
+			body.applyForceToCenter(new Vector2(3f, 0f).mul(movementDirection.x));
+
+			// apply jetpack
+			body.applyForceToCenter(new Vector2(0f, 15f).mul(movementDirection.y));
+		}
+
+	}
+
 	class Box2dDebugRendererTemplate extends EntityTemplateImpl {
 
 		@Override
@@ -55,6 +94,31 @@ public class NormalModeSceneTemplate {
 			Libgdx2dCamera camera = parameters.get("camera");
 			com.badlogic.gdx.physics.box2d.World physicsWorld = parameters.get("physicsWorld");
 			entity.addComponent(new RenderScriptComponent(new Box2dRenderDebugScript(camera, physicsWorld)));
+		}
+	}
+
+	class CharacterTemplate extends EntityTemplateImpl {
+
+		@Override
+		public void apply(Entity entity) {
+
+			float width = 0.5f;
+			float height = 1.5f;
+
+			Body body = bodyBuilder //
+					.fixture(bodyBuilder.fixtureDefBuilder() //
+							.boxShape(width * 0.5f, height * 0.5f)) //
+					.fixture(bodyBuilder.fixtureDefBuilder() //
+							.circleShape(new Vector2(0f, -0.75f), width * 0.5f)) //
+					.position(2f, 3f) //
+					.type(BodyType.DynamicBody) //
+					.fixedRotation() //
+					.userData(entity) //
+					.build();
+
+			entity.addComponent(new PhysicsComponent(new PhysicsImpl(body)));
+			entity.addComponent(new ScriptComponent(new CharacterControllerScript()));
+
 		}
 	}
 
@@ -67,7 +131,10 @@ public class NormalModeSceneTemplate {
 	// @Inject
 	ResourceManager<String> resourceManager;
 
-	EntityTemplateImpl box2dDebugRendererTemplate = new Box2dDebugRendererTemplate();
+	EntityTemplate box2dDebugRendererTemplate = new Box2dDebugRendererTemplate();
+	EntityTemplate characterTemplate = new CharacterTemplate();
+
+	private BodyBuilder bodyBuilder;
 
 	public void setResourceManager(ResourceManager<String> resourceManager) {
 		this.resourceManager = resourceManager;
@@ -77,12 +144,13 @@ public class NormalModeSceneTemplate {
 		final EventManager eventManager = new EventManagerImpl();
 
 		com.badlogic.gdx.physics.box2d.World physicsWorld = new com.badlogic.gdx.physics.box2d.World(new Vector2(0f, -10f), false);
+		bodyBuilder = new BodyBuilder(physicsWorld);
 
 		RenderLayers renderLayers = new RenderLayers();
 
 		Libgdx2dCamera worldCamera = new Libgdx2dCameraTransformImpl();
 
-		worldCamera.zoom(32f);
+		worldCamera.zoom(64f);
 
 		renderLayers.add(Layers.World, new RenderLayerSpriteBatchImpl(-100, 100, worldCamera));
 
@@ -91,8 +159,8 @@ public class NormalModeSceneTemplate {
 		scene.addUpdateSystem(new PhysicsSystem(physicsWorld));
 		scene.addUpdateSystem(new ReflectionRegistratorEventSystem(eventManager));
 
-//		scene.addRenderSystem(new SpriteUpdateSystem());
-//		scene.addRenderSystem(new RenderableSystem(renderLayers));
+		// scene.addRenderSystem(new SpriteUpdateSystem());
+		// scene.addRenderSystem(new RenderableSystem(renderLayers));
 		scene.addRenderSystem(new RenderScriptSystem());
 
 		scene.init();
@@ -103,15 +171,29 @@ public class NormalModeSceneTemplate {
 				.put("physicsWorld", physicsWorld) //
 				);
 
-		BodyBuilder bodyBuilder = new BodyBuilder(physicsWorld);
-
 		bodyBuilder //
 				.fixture(bodyBuilder.fixtureDefBuilder() //
 						.boxShape(30f, 0.5f)) //
 				.position(15f, 0.5f) //
 				.type(BodyType.StaticBody) //
 				.build();
-		
+
+		bodyBuilder //
+				.fixture(bodyBuilder.fixtureDefBuilder() //
+						.boxShape(1f, 0.1f)) //
+				.position(8f, 3.5f) //
+				.type(BodyType.StaticBody) //
+				.build();
+
+		bodyBuilder //
+				.fixture(bodyBuilder.fixtureDefBuilder() //
+						.boxShape(1f, 0.1f)) //
+				.position(4f, 4.5f) //
+				.type(BodyType.StaticBody) //
+				.build();
+
+		entityFactory.instantiate(characterTemplate);
+
 		Gdx.app.log(GameInformation.name, "Applying scene template...");
 	}
 
